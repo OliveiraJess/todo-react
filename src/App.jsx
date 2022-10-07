3596b86bdda5ba32d30fc2631e0aaa34cfbaf54a
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-10-07 10:16:08 -0300

    funcao erase, submit, onchange e onkeydown

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 
-const App = () => (
-  <section id='app' className='container'>
-    <header>
-      <h1 className='title'>Todo</h1>
-    </header>
-    <section className='main'>
-      <input className='new-todo' placeholder='O que precisa ser feito?'/>
-    </section>
+function App() {
+  const [value, setValue] = useState('');
 
-  </section>
-);
+  const ESCAPE_KEY = 27;
+  const ENTER_KEY = 13;
+
+  const erase = () => {
+    setValue('');
+  }
+
+  const submit = () => {
+    console.log(value);
+    erase();
+  }
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.which === ENTER_KEY) {
+      submit();
+
+    } else if (event.which === ESCAPE_KEY) {
+      erase();
+    }
+  };
+
+
+  return (
+    <section id='app' className='container'>
+      <header>
+        <h1 className='title'>Todo</h1>
+      </header>
+      <section className='main'>
+        <input className='new-todo'
+          placeholder='O que precisa ser feito?'
+          value={value}
+          onChange={onChange}
+          onKeyDown={onKeyDown}
+        />
+      </section>
+
+    </section>
+  )
+};
 
 export default App;
